58f25040fb02d56773ade0a4997542fdd4600c86
Oleksandr Sotnikov <oleksandr.v.sotnikov@gmail.com>
2020-04-20 22:46:53 +0300

    Add multi words support for a script

--- p_t.sh
+++ p_t.sh
@@ -4,15 +4,15 @@ export PS1
 PATH=.:$PATH
 export PATH
 
-type xclip 1>/dev/null 2>&1 && echo -n $1 | xclip -sel clip
+type xclip 1>/dev/null 2>&1 && echo -n "${1}" | xclip -sel clip
 
-echo -n $2 >phonetics/$1
-echo -n $3 >translations/$1
+echo -n "${2}" >phonetics/"${1}"
+echo -n "${3}" >translations/"${1}"
 
-audio=sound/$1.mp3  
+audio="sound/"${1}".mp3"  
 
-[ -f ~/Downloads/$1.mp3 ] && mv -f ~/Downloads/$1.mp3 $audio
+[ -f ~/Downloads/"${1}".mp3 ] && mv -f ~/Downloads/"${1}".mp3 $audio
 
-[ -f $audio ] && mplayer >/dev/null 2>&1 $audio
-[ ! -z $4 ] && touch words/active/$1
+[ -f "${audio}" ] && mplayer >/dev/null 2>&1 "${audio}"
+[ ! -z "${4}" ] && touch words/active/"${1}"
  
