f80d53454636f3c226635ecb68a2f7d441adb0d1
Oleksandr Sotnikov <oleksandr.v.sotnikov@gmail.com>
2020-09-29 14:09:30 +0300

    Correct script save mp3 files for word phrases

--- p_t.sh
+++ p_t.sh
@@ -7,9 +7,9 @@ type xclip 1>/dev/null 2>&1 && echo -n "${1}" | xclip -sel clip
 echo -n "${2}" >phonetics/"${1}"
 echo -n "${3}" >translations/"${1}"
 
-audio="sound/"${1}".mp3"  
+audio=sound/"${1}".mp3
 
-[ -f ~/Downloads/"${1}".mp3 ] && mv -f ~/Downloads/"${1}".mp3 $audio
+[ -f ~/Downloads/"${1}".mp3 ] && mv -f ~/Downloads/"${1}".mp3 "${audio}"
 
 [ -f "${audio}" ] && mplayer >/dev/null 2>&1 "${audio}"
 [ ! -z "${4}" ] && touch words/active/"${1}"
