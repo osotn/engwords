7ec883a1f57c38933851d94d3dfa588603e89436
Oleksandr Sotnikov <oleksandr.v.sotnikov@gmail.com>
2021-02-11 14:17:53 +0200

    Add xT/yX frequency number rate index in px.sh (xT is part of yT)

--- px.sh
+++ px.sh
@@ -9,4 +9,6 @@ echo $1 | run.sh $RUN_FLAGS  | grep 4T >/dev/null && \
 	cat words/ielts_general_4000.txt               | grep -n $1
 echo $1 | run.sh $RUN_FLAGS  | grep 5T >/dev/null && \
 	cat words/toefl_5000.txt                       | grep -n $1
+echo $1 | run.sh $RUN_FLAGS  | egrep "xT|yT" >/dev/null && \
+	cat words/google-20000-english.txt             | grep -n $1
 
