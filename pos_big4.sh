1082c46f5d0c2be493b9f8c5105556ffe6fcdd80
Oleksandr Sotnikov <oleksandr.v.sotnikov@gmail.com>
2021-03-05 17:28:03 +0200

    Add pos print text in x3 and x4 size

--- pos_big4.sh
+++ pos_big4.sh
@@ -1,3 +1,3 @@
 # Use fontA (12x24, default), it takes 32 character per line.
 # Only fold from busybox can work correctly with UTF-8.
-busybox fold -w 16 -s |  ./utf8_to_pos | sed '1s/^/\x1D\x21\x11/' | sed '$ s/$/\x1D\x21\x00/' >> /dev/usb/lp0
+busybox fold -w 8 -s |  ./utf8_to_pos | sed '1s/^/\x1D\x21\x33/' | sed '$ s/$/\x1D\x21\x00/' >> /dev/usb/lp0
