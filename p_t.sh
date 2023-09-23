0605e413f0ffcbcefece13fefdb577ec9b101225
Oleksandr Sotnikov <oleksandr.v.sotnikov@gmail.com>
2023-09-23 21:22:11 +0300

    Replace spaces in mp3 file on "+"s for phase words

--- p_t.sh
+++ p_t.sh
@@ -7,10 +7,18 @@ type xclip 1>/dev/null 2>&1 && echo -n "${1}" | xclip -sel clip
 echo -n "${2}" >phonetics/"${1}"
 echo -n "${3}" >translations/"${1}"
 
-audio=sound/"${1}".mp3
+audio="${1}".mp3
 
-[ -f ~/Downloads/"${1}".mp3 ] && mv -f ~/Downloads/"${1}".mp3 "${audio}"
+# Replace spaces on "+" for mp3 name
+if  echo ${audio} | grep -q "+"; then
+    old_audio="`echo ${audio} | tr "+" " "`"
+    [ -f  ~/Downloads/"${old_audio}" ] && mv -f ~/Downloads/"${old_audio}"  ~/Downloads/"${audio}"
+fi
 
-[ -f "${audio}" ] && mplayer >/dev/null 2>&1 "${audio}"
+[ -f ~/Downloads/"${audio}" ] && mv -f ~/Downloads/"${audio}" sound/"${audio}"
+
+
+[ -f sound/"${audio}" ] && mplayer >/dev/null 2>&1 sound/"${audio}"
+
 [ ! -z "${4}" ] && touch words/active/"${1}"
  
