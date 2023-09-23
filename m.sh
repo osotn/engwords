d1eb59708d7fdc5a0fcd999f7f1608924e860428
Oleksandr Sotnikov <oleksandr.v.sotnikov@gmail.com>
2023-09-23 21:30:30 +0300

    Replace spaces in mp3 file on "+"s for phase words

--- m.sh
+++ m.sh
@@ -2,9 +2,16 @@
 
 for word in "$@"
 do
-    audio=sound/"${word}".mp3  
-    [ -f ~/Downloads/"${word}".mp3 ] && mv -f ~/Downloads/"${word}".mp3 "$audio"
-    [ -f "${audio}" ] && mplayer >/dev/null 2>&1 "${audio}"
+    audio="${word}".mp3
+
+    # Replace spaces on "+" for mp3 name
+    if  echo ${audio} | grep -q "+"; then
+        old_audio="`echo ${audio} | tr "+" " "`"
+        [ -f  ~/Downloads/"${old_audio}" ] && mv -f ~/Downloads/"${old_audio}"  ~/Downloads/"${audio}"
+    fi
+
+    [ -f ~/Downloads/"${word}".mp3 ] && mv -f ~/Downloads/"${word}".mp3 sound/"$audio"
+    [ -f sound/"${audio}" ] && mplayer >/dev/null 2>&1 sound/"${audio}"
     sleep 1
 done
 
